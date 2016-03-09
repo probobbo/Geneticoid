061927f7ddedcb218c412c7773af328f51305c1e
probobbo <probobbo@gmail.com>
2016-03-09 17:15:45 +0100

    Prima bozza parametri evolutivi

--- Assets/Scripts/EnemyCircleMovement.cs
+++ Assets/Scripts/EnemyCircleMovement.cs
@@ -7,7 +7,6 @@ public class EnemyCircleMovement : InterfaceEnemyMovement
     public Vector3 axis = Vector3.forward;
     public Vector3 desiredPosition;
     public float radius = 1.0f;
-    public float radiusSpeed = 0.5f;
     public float rotationSpeed = 40.0f;
     Transform player;
     float speed;
@@ -22,7 +21,7 @@ public class EnemyCircleMovement : InterfaceEnemyMovement
     {
         transform.RotateAround(player.position, axis, rotationSpeed * Time.deltaTime);
         desiredPosition = (transform.position - player.position).normalized * radius + player.position;
-        transform.position = Vector3.MoveTowards(transform.position, desiredPosition, Time.deltaTime * radiusSpeed);
+        transform.position = Vector3.MoveTowards(transform.position, desiredPosition, Time.deltaTime * speed);
         Vector3 dir = player.position - transform.position;
         float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.AngleAxis(angle - 90, Vector3.forward);
